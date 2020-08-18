6c69465c1f808bf57f902522cb623696fa22e026
TranPhuongNam1997 <tranphuongnam17041997@gmail.com>
2020-08-18 07:35:17 +0700

    baitapvenha1208

--- Bai tap 1/trangchu.js
+++ Bai tap 1/trangchu.js
@@ -33,6 +33,12 @@
 					console.log("Not Oke")// show messsage tai khoan mat khau ko dung Nam tu xur ly
 				}
 				else{
+
+					//checkvalidate
+					checkAccount();
+					checkPassword();
+
+
 					var taiKhoanJson = JSON.stringify(taiKhoan);
 					localStorage.setItem("AccountLoginNow",taiKhoanJson);
 					window.location.href = "chitiet.html";
@@ -48,4 +54,53 @@
 			}
 
 
+		}
+
+		document.addEventListener('DOMContentLoaded',function(){
+		
+			HanhDongDangNhap();
+
+			
+
+
+			
+			
+		});
+
+		//khai bao bien
+		var accountVl = document.getElementById("inputAccount");
+		var passwordVl = document.getElementById("inputPassword");
+
+
+		//ham kiem tra tai khoan
+
+		function checkAccount(){
+			
+			var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+			if(accountVl.value == ""){
+				document.getElementById("er-accountnotnull").style.display = "block";
+				document.getElementById("er-account").style.display = "none";
+			}
+			
+			else if(!re.test(accountVl.value) &&  accountVl.value != "") {
+				document.getElementById("er-accountnotnull").style.display = "none";
+			    document.getElementById("er-account").style.display = "block";
+			} 
+			
+			else {
+			    document.getElementById("er-accountnotnull").style.display = "none";
+			    document.getElementById("er-account").style.display = "none";
+			}
+		}
+		
+		//ham kiem tra mat khau
+		function checkPassword(){
+			if (passwordVl.value == "") {
+			    document.getElementById("er-password").style.display = "block";
+			} 
+
+			else {
+			    document.getElementById("er-password").style.display = "none";
+			}
 		}
