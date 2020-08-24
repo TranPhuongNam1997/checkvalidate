465d4a4c82f70a0d0f1452d8efd8f37ad1109ec3
TranPhuongNam1997 <tranphuongnam17041997@gmail.com>
2020-08-24 23:58:54 +0700

    update2408

--- Bai tap 1/quenmatkhau.js
+++ Bai tap 1/quenmatkhau.js
@@ -16,54 +16,95 @@
 
 			 // cu load lai trang no se chay vao day dung chua
 		document.addEventListener('DOMContentLoaded',function(){
-			var listTk = [
-			{
-				id:1,
-				ho:"Nam",
-				ten: "23",
-				namSinh: '1997',
-				gmail: "nam@gmail.com",
-				matKhau:'123456',
-				gmailPhu: 'namtb@gmail.com',
-				diaChi: 'tb',
-				taikhoan:'namdaica',
-				soDt: '0976809926'
-			},
-			{
-				id:2,
-				ho:"Minh",
-				ten: "234",
-				namSinh: '1997',
-				gmail: "BG",
-				matKhau:'123456',
-				gmailPhu: 'minh@gmail.com',
-				diaChi: 'tb',
-				taikhoan:'minhdaica',
-				soDt: '0976809927'
-			},
-			{
-				id:3,
-				ho:"Tran",
-				ten: "232",
-				namSinh: '1997',
-				gmail: "tran@gmail.com",
-				matKhau:'123456',
-				gmailPhu: 'minhnb@gmail.com',
-				diaChi: 'nb',
-				taikhoan:'trandaica',
-				soDt: '0976809928'
-			}
-			];
-			console.log(listTk)
-			mailnhapvao="nam@gmail.com";
-			matkhaumoi="123abc";
+			// var listTk = [
+			// {
+			// 	id:1,
+			// 	ho:"Nam",
+			// 	ten: "23",
+			// 	namSinh: '1997',
+			// 	gmail: "nam@gmail.com",
+			// 	matKhau:'123456',
+			// 	gmailPhu: 'namtb@gmail.com',
+			// 	diaChi: 'tb',
+			// 	taikhoan:'namdaica',
+			// 	soDt: '0976809926'
+			// },
+			// {
+			// 	id:2,
+			// 	ho:"Minh",
+			// 	ten: "234",
+			// 	namSinh: '1997',
+			// 	gmail: "BG",
+			// 	matKhau:'123456',
+			// 	gmailPhu: 'minh@gmail.com',
+			// 	diaChi: 'tb',
+			// 	taikhoan:'minhdaica',
+			// 	soDt: '0976809927'
+			// },
+			// {
+			// 	id:3,
+			// 	ho:"Tran",
+			// 	ten: "232",
+			// 	namSinh: '1997',
+			// 	gmail: "tran@gmail.com",
+			// 	matKhau:'123456',
+			// 	gmailPhu: 'minhnb@gmail.com',
+			// 	diaChi: 'nb',
+			// 	taikhoan:'trandaica',
+			// 	soDt: '0976809928'
+			// }
+			// ];
+
+
+
+
+
+			// console.log(listTk)
+
+
+			var danhSachTaiKhoanCuaHeThong3 = JSON.parse(localStorage.getItem("ListAccount"));
+			console.log(danhSachTaiKhoanCuaHeThong3);
+
+
+
+			// mailnhapvao="nam@gmail.com";
+			// matkhaumoi="123abc";
 			// thay mat khau cho mail tuong ung
 
-			var tk = listTk.find(taikhoanNew => taikhoanNew.gmail == mailnhapvao)
-			tk.matKhau = matkhaumoi;
-			console.log(listTk)
+			// var tk = listTk.find(taikhoanNew => taikhoanNew.gmail == mailnhapvao)
+			// tk.matKhau = matkhaumoi;
+			// console.log(listTk)
+
 			// push len la xong localsotgate
 
+
+			var nutXacThuc = document.getElementById('btnXacThuc');
+			var mailNhap = document.getElementById('inputAccount');
+			var mkMoi = document.getElementById('inputPassword');
+
+			nutXacThuc.onclick = function(){
+				var tk = danhSachTaiKhoanCuaHeThong3.find(taikhoanNew => taikhoanNew.gmail == mailNhap.value);
+
+				
+
+				tk.matKhau = mkMoi.value;
+
+				danhSachTaiKhoanCuaHeThong3.push(tk);
+
+				localStorage.removeItem("ListAccount");
+				// localStorage.setItem("ListAccount",listUpdate);
+
+				var listUpdateMk = JSON.stringify(danhSachTaiKhoanCuaHeThong3);
+				localStorage.setItem("ListAccount",listUpdateMk);
+
+
+
+				var danhSachTaiKhoanCuaHeThong4 = JSON.parse(localStorage.getItem("ListAccount"));
+				console.log(danhSachTaiKhoanCuaHeThong4);
+			}
 			
+
+
+
 		});
 		
